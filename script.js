265b516049d53fa456ff9fd296333461cdf900c5
Jason Lin <122318900+jasonlinacct@users.noreply.github.com>
2023-09-27 22:58:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-// JavaScript code for shuffling the colors of the Rubik's Cube faces
+// JavaScript code for shuffling the colors of the Rubik's Cube faces with a constraint
 
 const faces = document.querySelectorAll('.cube-face');
 const shuffleButton = document.getElementById('shuffle');
 
-const cubeColors = ['#ff6347', '#0000cd', '#ffff00', '#008000', '#ff4500', '#ffffff']; // Colors for the cube faces
+// Define the six possible colors
+const cubeColors = ['#ff6347', '#0000cd', '#ffff00', '#008000', '#ff4500', '#ffffff'];
 
 shuffleButton.addEventListener('click', () => {
-    faces.forEach((face) => {
-        // Randomly select a color from the cubeColors array
-        const randomColor = cubeColors[Math.floor(Math.random() * cubeColors.length)];
-        
-        // Apply the selected color as the background
-        face.style.backgroundColor = randomColor;
+    // Shuffle the colors randomly
+    shuffleArray(cubeColors);
+
+    // Assign the shuffled colors to the cube faces
+    faces.forEach((face, index) => {
+        face.style.backgroundColor = cubeColors[index];
     });
 });
+
+// Function to shuffle an array (Fisher-Yates algorithm)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
