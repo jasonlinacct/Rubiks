492795c4a39f465b6a00f9f3e946da02a2fff453
Jason Lin <122318900+jasonlinacct@users.noreply.github.com>
2023-09-27 22:56:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// JavaScript code for cube shuffling (basic example)
+// JavaScript code for shuffling the colors of the Rubik's Cube faces
 
 const faces = document.querySelectorAll('.cube-face');
 const shuffleButton = document.getElementById('shuffle');
 
+const cubeColors = ['#ff6347', '#0000cd', '#ffff00', '#008000', '#ff4500', '#ffffff']; // Colors for the cube faces
+
 shuffleButton.addEventListener('click', () => {
     faces.forEach((face) => {
-        // Change the content of each face when shuffling
-        face.textContent = Math.floor(Math.random() * 6) + 1;
+        // Randomly select a color from the cubeColors array
+        const randomColor = cubeColors[Math.floor(Math.random() * cubeColors.length)];
+        
+        // Apply the selected color as the background
+        face.style.backgroundColor = randomColor;
     });
 });
